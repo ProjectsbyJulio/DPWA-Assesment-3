963172638c52e6c0616e77253470268e23b59684
Julio Canizalez <juliocanizalez@outlook.com>
2021-06-08 01:55:34 -0600

    [ADD] Files, routes, styles

--- DPWA_Assessment/Controllers/CategoriaController.cs
+++ DPWA_Assessment/Controllers/CategoriaController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -27,6 +28,41 @@ namespace DPWA_Assessment.Controllers
             }
 
             return View(listaCategorias);
+        }
+
+        public ActionResult InsertarCategoria()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult InsertarCategoria(CategoriaViewModel model, HttpPostedFileBase file)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    if(file != null)
+                    {
+                        string path = Path.Combine(Server.MapPath("~/Assets/Images"), Path.GetFileName(file.FileName));
+                        file.SaveAs(path);
+                    }
+                    using (juegoJECSEntities db = new juegoJECSEntities())
+                    {
+                        var data = new categoria();
+                        data.categoria1 = model.Categoria;
+                        data.imagenCat = model.imagenCat;
+
+                        db.categorias.Add(data);
+                        db.SaveChanges();
+                    }
+                }
+                return Redirect("Categoria/");
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
         }
     }
 }
--- DPWA_Assessment/Models/ViewModel/CategoriaViewModel.cs
+++ DPWA_Assessment/Models/ViewModel/CategoriaViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,8 +8,14 @@ namespace DPWA_Assessment.Models.ViewModel
 {
     public class CategoriaViewModel
     {
-        public int Id { get; set;}
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Nombre")]
         public string Categoria { get; set; }
+        [Required]
+        [DataType(DataType.Upload)]
+        [Display(Name = "Imagen")]
         public string imagenCat { get; set; }
     }
 }
